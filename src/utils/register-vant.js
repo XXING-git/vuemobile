034fb9f9f1f83fdbb22a2ad002ea3e72e71ee7d6
dhx <you@example.com>
2020-01-08 18:03:04 +0800

    请求登录

--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 
-import { Button, Cell, CellGroup, NavBar, Field } from 'vant'
+import { Button, Cell, CellGroup, NavBar, Field, Icon } from 'vant'
 
 Vue.use(Button)
   .use(Cell)
   .use(CellGroup)
   .use(NavBar)
   .use(Field)
+  .use(Icon)
