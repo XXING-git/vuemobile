7c83a13878135f21cd8ae4b6dc757eb9d8d6d47f
dhx <you@example.com>
2020-01-11 20:44:19 +0800

    展示当前登录用户信息

--- src/api/user.js
+++ src/api/user.js
@@ -3,6 +3,7 @@
  */
 import request from '@/utils/request'
 
+// 用户登录
 export const login = data => {
   // 具体的请求代码
   return request({
@@ -22,5 +23,13 @@ export const getSmsCode = mobile => {
   return request({
     method: 'GET',
     url: `/app/v1_0/sms/codes/${mobile}`
+  })
+}
+
+// 获取当前登录用户信息
+export const getUserInfo = () => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user'
   })
 }
