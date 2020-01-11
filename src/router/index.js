a36c886635abf9bd6b2c23706f1c1b3a802fdfde
dhx <you@example.com>
2020-01-11 17:01:20 +0800

    个人中心-TarBar路由组件

--- src/router/index.js
+++ src/router/index.js
@@ -8,10 +8,38 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/login',
-    component: Login
+    name: 'login',
+    component: () => import('@/views/login')
+  },
+  {
+    path: '/',
+    // 有默认子路由的不需要配置 name
+    // name: 'tab-bar',
+    component: () => import('@/views/tab-bar'),
+    children: [
+      {
+        path: '', // 默认子路由
+        name: 'home',
+        component: () => import('@/views/home')
+      },
+      {
+        path: 'qa',
+        name: 'qa',
+        component: () => import('@/views/qa')
+      },
+      {
+        path: 'video',
+        name: 'video',
+        component: () => import('@/views/video')
+      },
+      {
+        path: 'my',
+        name: 'my',
+        component: () => import('@/views/my')
+      }
+    ]
   }
 ]
-
 const router = new VueRouter({
   routes
 })
--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 
-import { Button, Cell, CellGroup, NavBar, Field, Icon, Toast, CountDown } from 'vant'
+import { Button, Cell, CellGroup, NavBar, Field, Icon, Toast, CountDown, Tabbar, TabbarItem } from 'vant'
 
 Vue.use(Button)
   .use(Cell)
@@ -10,3 +10,5 @@ Vue.use(Button)
   .use(Icon)
   .use(Toast)
   .use(CountDown)
+  .use(Tabbar)
+  .use(TabbarItem)
